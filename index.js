2e78a7299de77bf24f3f0e9490ce094ec1ed39a7
Xylon Funes <xyfullns@gmail.com>
2023-03-21 22:19:18 -0400

    While and iteration

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(numbers){
+   while(numbers >= 0){
+    console.log(numbers--);
+   } 
+}
+
+function writeCards(array, name){
+    let i = 0;
+    let newArray =[];
+
+    while(i < array.length){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`)
+    i++;
+    }
+    return newArray;
+}
